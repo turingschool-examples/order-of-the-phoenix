197567652b2df97f6c5f32d51f6241c9dcae022d
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-19 11:12:38 -0600

    Adds character information to index

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -8,11 +8,9 @@ describe 'Welcome index page' do
 
   it 'sends me to a new page when I click submit on drop down box' do
     visit '/'
-
-  
+ 
     select 'Slytherin', from: 'house'
     click_on 'Search For Members'
-
 
     expect(page).to have_current_path('/search?utf8=%E2%9C%93&house=Slytherin&commit=Search+For+Members')
   end
