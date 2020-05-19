c40d3a851144d88c8c1f48ae693e70e71375f893
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-19 10:13:23 -0600

    Start to potter services

---
+++ app/services/potter_service.rb
@@ -0,0 +1,19 @@
+class PotterService
+
+  def members
+  end
+
+  private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:key] = ENV['POTTER_API_KEY']
+    end
+  end
+end
