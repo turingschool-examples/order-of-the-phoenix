98ec1354eb4f29b0436582369f33a339ef286621
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-19 10:07:18 -0600

    Add index spex for welcome

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 describe 'Welcome index page' do
   it 'has a drop down menu to select the house' do
     visit '/'
-    click on 'Slytherin', from: 'hogwarts_house'
+    select 'Slytherin', from: 'house'
+  end
+
+  it 'sends me to a new page when I click submit on drop down box' do
+    visit '/'
+
+  
+    select 'Slytherin', from: 'house'
+    click_on 'Search For Members'
+
+
+    expect(page).to have_current_path('/search?utf8=%E2%9C%93&house=Slytherin&commit=Search+For+Members')
   end
 end
